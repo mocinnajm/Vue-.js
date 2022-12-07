a08fb66a4dfb7aa57949e04044f1135287f2d902
mocinnajm <alge1257@gmail.com>
2022-12-08 00:06:02 +0100

    agregar proyecto

---
+++ index.js
@@ -0,0 +1,17 @@
+const app=Vue.createApp({
+    data(){
+
+         return{
+            titulo:'Primera app',
+            subtitulo:'Reactividad en vue',
+            paragrafo:'Vue.js es un framework de JavaScript de código abierto para la construcción de interfaces de usuario y aplicaciones de una sola página. Fue creado por Evan You, y es mantenido por él y por el resto de los miembros activos del equipo central que provienen de diversas empresas como Netlify y Netguru',
+            imagen:'./fotos/okay.jpg',
+            link:'https://vuejs.org/',
+            saludo:'mohcine'
+         }
+    }
+
+
+
+
+})
